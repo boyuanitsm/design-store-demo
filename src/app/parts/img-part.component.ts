e759d23af4a90ef3e62063b8252afe92f764a88a
hookszhang <hua.zhang@boyuanitsm.com>
2017-04-14 11:32:59 +0800

    Add component styles

--- src/app/parts/img-part.component.ts
+++ src/app/parts/img-part.component.ts
@@ -5,7 +5,12 @@ import {Component, Input} from '@angular/core';
 import {PartComponent} from "./part.component";
 
 @Component({
-  template: '<img src="{{data.src}}"/>'
+  template: '<img class="img-part-component" src="{{data.src}}"/>',
+  styles: [`
+    .img-part-component {
+      width: 100%;
+    }
+  `]
 })
 export class ImgPartComponent implements PartComponent {
   @Input() data: any;
--- src/app/parts/text-part.component.ts
+++ src/app/parts/text-part.component.ts
@@ -5,7 +5,12 @@ import {Component, Input} from '@angular/core';
 import {PartComponent} from "./part.component";
 
 @Component({
-  template: '<p>{{data.text}}</p>'
+  template: '<p class="text-part-component">{{data.text}}</p>',
+  styles: [`
+    .text-part-component {
+      color: #546E7A;
+    }
+  `]
 })
 export class TextPartComponent implements PartComponent {
   @Input() data: any;
